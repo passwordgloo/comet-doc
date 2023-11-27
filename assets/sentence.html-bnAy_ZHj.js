20620e4390b1c27a221582e171c906b5f3c79385
passwordgloo <passwordgloo@163.com>
2023-11-27 14:56:40 +0000

    Deploying to gh-pages from @ passwordgloo/comet-doc@d0ffa9c2cece8cfde883acaaa79683adaaf4d5b4 🚀

---
+++ assets/sentence.html-bnAy_ZHj.js
@@ -0,0 +1,31 @@
+import{_ as r}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as d,o as l,c,d as a,w as i,e as s,a as e}from"./app-_7jqBeMZ.js";const h={},u=s('<h2 id="表达式和语句" tabindex="-1"><a class="header-anchor" href="#表达式和语句" aria-hidden="true">#</a> 表达式和语句</h2><div class="hint-container tip"><p class="hint-container-title">名词解释</p><p>表达式是一组代码的集合，Javascript解释器会将其计算出一个结果</p><p>语句是js整句或命令，js语句是以分号结束</p></div><div class="hint-container warning"><p class="hint-container-title">表达式和语句区别</p><p>表达式计算出一个值，但语句用来自行以使某件事发生（做什么事）</p></div><h2 id="流程控制语句" tabindex="-1"><a class="header-anchor" href="#流程控制语句" aria-hidden="true">#</a> 流程控制语句</h2><h3 id="顺序" tabindex="-1"><a class="header-anchor" href="#顺序" aria-hidden="true">#</a> 顺序</h3><p>流程按照线性顺序执行</p><h3 id="分支" tabindex="-1"><a class="header-anchor" href="#分支" aria-hidden="true">#</a> 分支</h3><h4 id="if分支语句" tabindex="-1"><a class="header-anchor" href="#if分支语句" aria-hidden="true">#</a> if分支语句</h4>',8),v=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`if(条件){
+	满足条件执行的代码
+}
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),o=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`if(条件){
+	满足条件要执行的代码
+}
+else {
+	不满足条件执行的代码
+}
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),m=s(`<h4 id="三元运算符" tabindex="-1"><a class="header-anchor" href="#三元运算符" aria-hidden="true">#</a> 三元运算符</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>条件?满足条件执行的代码:不满足条件执行的代码
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h4 id="switch语句" tabindex="-1"><a class="header-anchor" href="#switch语句" aria-hidden="true">#</a> switch语句</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>switch(条件){
+	case 值1:
+		代码1
+		break
+	case 值2
+		代码2
+		break
+	case 值3
+		代码3
+		break
+	default
+		代码n
+		break        
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="循环" tabindex="-1"><a class="header-anchor" href="#循环" aria-hidden="true">#</a> 循环</h3><div class="hint-container danger"><p class="hint-container-title">终止条件</p><p>循环一定要有终止条件，否则会死循环</p></div><h4 id="while" tabindex="-1"><a class="header-anchor" href="#while" aria-hidden="true">#</a> while</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>while(循环条件){
+	要重复执行的代码(循环体)
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="for" tabindex="-1"><a class="header-anchor" href="#for" aria-hidden="true">#</a> for</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>for(声明记录循环次数的变量;循环条件；变化值){
+	循环体
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="循环结束" tabindex="-1"><a class="header-anchor" href="#循环结束" aria-hidden="true">#</a> 循环结束</h4><p>continue: 结束本次循环，继续下次循环</p><p>break:跳出循环</p>`,13);function b(p,x){const n=d("CodeGroupItem"),t=d("CodeGroup");return l(),c("div",null,[u,a(t,null,{default:i(()=>[a(n,{title:"单层"},{default:i(()=>[v]),_:1}),a(n,{title:"双层"},{default:i(()=>[o]),_:1})]),_:1}),m])}const g=r(h,[["render",b],["__file","sentence.html.vue"]]);export{g as default};
