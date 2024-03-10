90b022646ccb0c5d658f6e26aa40eb2327f6a975
passwordgloo <passwordgloo@163.com>
2024-03-10 15:15:42 +0800

    添加algolia搜索(0.0.0.0.3.2)

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -148,6 +148,63 @@ export default hopeTheme({
       // sandpack: true,
     },
 
+    docsearch:({
+      appId: "G9OIK8333O",
+      apiKey: "efb5317f69b80cdcabfb520352731e09",
+      indexName: "Doc_comet",
+      locales: {
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+        // "/en/": {
+        //   placeholder: "Search Documentation",
+        //   translations: {
+        //     button: {
+        //       buttonText: "Search Documentation",
+        //     },
+        //   },
+        // },
+      },
+    }),
+
     // install @vuepress/plugin-pwa and uncomment these if you want a PWA
     // pwa: {
     //   favicon: "/favicon.ico",
